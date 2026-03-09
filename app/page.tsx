c0ed8c8a3c92921e571f26e59a42cfee65ac36d1
Carlos Amorós <camorost@gmail.com>
2026-03-09 18:52:55 -0500

    Update homepage title to Full Stack Software Engineer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex w-full flex-col items-center justify-center space-y-12">
       <h1 className="md:text-5xl">Carlos Amorós</h1>
-      <p className="text-2xl md:text-4xl">Full stack Developer</p>
+      <p className="text-2xl md:text-4xl">Full Stack Software Engineer</p>
     </div>
   );
 }
